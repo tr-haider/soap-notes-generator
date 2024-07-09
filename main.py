457a7b892e524b221e091dab2c9771c138e20a6c
tr-haider <mhaidershahab@technologyrivers.com>
2024-07-09 17:41:11 +0500

    Soap notes creation feature

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ import pandas as pd
 import fitz  # PyMuPDF
 from moviepy.editor import VideoFileClip
 from datetime import datetime
+from streamlit_mic_recorder import speech_to_text
+from pydub import AudioSegment
+from concurrent.futures import ThreadPoolExecutor,as_completed
+import whisper
+
 # Load environment variables
 load_dotenv()
 
@@ -48,13 +53,65 @@ def extract_text_from_pdf(pdf_path):
     for page in doc:
         text += page.get_text()
     return text
-def extract_text_from_audio(audio_file):
-    transcription = openai.audio.transcriptions.create(
-        model="whisper-1",
-        file=audio_file
-    )
-    return transcription.text
+def create_chunks(wav_file):
+    audio = AudioSegment.from_wav(wav_file)
+    chunk_length_ms = 60000
+    transcribed_text = ""
+    chunk_file_names = []
+    # create a directory to store the audio chunks.
+    try:
+        os.mkdir('audio_chunks')
+    except(FileExistsError):
+        pass
+    os.chdir('audio_chunks')
+    num_chunks = len(audio) // chunk_length_ms + 1
+    for i in range(num_chunks):
+        start_time = i * chunk_length_ms
+        end_time = start_time + chunk_length_ms
 
+        # Extract the chunk
+        audio_chunk = audio[start_time:end_time]
+
+        # export audio chunk and save it in the current directory.
+        chunk_filename = f"chunk{i}.wav"
+        audio_chunk.export(chunk_filename, bitrate='192k', format="wav")
+
+        # Debug: Print information about the chunk
+        print(f"Saving chunk{i}.wav: duration {len(audio_chunk)}ms")
+
+        # get the name of the newly created chunk in the AUDIO_FILE variable for later use.
+        chunk_file_names.append(chunk_filename)
+    return chunk_file_names
+def process_chunk(chunk):
+    return extract_text_from_speech(chunk)
+def extract_text_from_speech(audio_file):
+    model = whisper.load_model("base")
+    result = model.transcribe(audio_file)
+    transcribed_text = result["text"]
+    return transcribed_text
+
+def convert_audio_to_wav(audio_file):
+    audio = AudioSegment.from_file(audio_file)
+    wav_file = audio_file.split(".")[0] + ".wav"
+    audio.export(wav_file, format="wav")
+    return wav_file
+
+def extract_text_from_audio(audio_file):
+    audio_file = str(audio_file)
+    chunks = create_chunks(audio_file)
+    num_workers = len(chunks)  # Adjust the number of workers as needed
+    text = ""
+    with ThreadPoolExecutor(max_workers=num_workers) as executor:
+        future_to_chunk = {executor.submit(process_chunk, chunk): chunk for chunk in chunks}
+
+        for future in as_completed(future_to_chunk):
+            chunk = future_to_chunk[future]
+            try:
+                text += future.result()
+            except Exception as exc:
+                print(f'Chunk {chunk} generated an exception: {exc}')
+
+    return text
 
 # Function to extract text from video
 def extract_text_from_video(video_path):
@@ -67,6 +124,87 @@ def extract_text_from_video(video_path):
     # Transcribe audio to text
     text = extract_text_from_audio(audio_path)
     return text
+
+def get_soap_notes_prompt(notes_text):
+    prompt = (
+        f"""Create well-structured and effective SOAP notes with the following rules for the following patient visit. Include complete headings such as Subjective, Objective, Assessment, and Plan."
+              Patient visit : \n\n{notes_text}\n\n
+              Rules : 
+              1. In Subjective section,follow these points : 
+                 Document the patient's primary symptoms (e.g., fever, cough, sore throat).
+                 Include the duration of symptoms.
+                 Note any relevant exposure history, such as contact with sick individuals.
+                 Record any other related symptoms (e.g., chills, muscle aches).
+                 Mention any treatments the patient has been using (e.g., medications taken).
+
+              2.In Objective section,summarize and cover the following points:
+                Vital Signs: Record specific vital signs, such as temperature, respiratory rate, heart rate, and blood pressure.
+                Physical Examination:
+                   HEENT: Describe findings related to the head, eyes, ears, nose, and throat (e.g., erythema in the throat).
+                   Respiratory: Note findings from lung examination (e.g., clear to auscultation bilaterally).
+                   Cardiovascular: Document heart rate, rhythm, and any abnormal sounds (e.g., S1 and S2 normal).
+                   Musculoskeletal: Include findings if relevant.
+                   Neurological: Include findings if relevant.
+                   Dermatological: Include findings if relevant.
+                   Gastrointestinal: Include findings if relevant.
+                   Diagnostic Tests and Labs: Note any relevant test results (e.g., N/A if no tests were done).
+
+              3.In Assessment section,follow these points : 
+                Assessment:
+                      Identify the condition or diagnosis based on the subjective and objective data (e.g., Acute pharyngitis).
+                      Note specific findings that support the diagnosis (e.g., erythema in the throat).
+
+              4. In Plan section,follow these points :
+                    Recommend any medications (e.g., continue taking Tylenol as needed).
+                    Encourage adequate hydration and rest.
+                    Advise on monitoring symptoms and provide guidance on follow-up if symptoms worsen or do not improve within a specified timeframe (e.g., 5-7 days).
+
+              You can use this soap notes as example.The response format should be like that: 
+
+              Subjective:
+
+              The patient, John, presents with a chief complaint of fever, cough, and sore throat. He reports that these symptoms began two days ago. He denies being around anyone sick recently but mentions that he has been going to work. In addition to his primary symptoms, John also experiences chills and muscle itches. He states that his temperature at home reached 102 degrees Fahrenheit. He denies any shortness of breath or wheezing. John has been taking Tylenol a couple of times per day to manage his symptoms.
+
+              Objective:
+
+               - Vital Signs
+                   - Temperature: 102 degrees Fahrenheit
+                   - Respiratory rate, heart rate, blood pressure: N/A
+
+               - Physical Examination
+                  - HEENT: Erythema in the back of the throat
+                  - Respiratory: Lungs clear to auscultation bilaterally
+                  - Cardiovascular: Heart rate and rhythm regular, S1 and S2 normal
+                  - Musculoskeletal: N/A
+                  - Neurological: N/A
+                  - Dermatological: N/A
+                  - Gastrointestinal: N/A
+
+               - Diagnostic Test Results and Labs
+                  - N/A
+
+              Assessment & Plan:
+
+                 1. Acute pharyngitis:
+                  - Erythema noted in the back of the throat
+                 Plan:
+                    - Continue taking Tylenol as needed for fever and pain relief
+                    - Encourage adequate hydration and rest
+                    - Monitor symptoms and return for follow-up if symptoms worsen or do not improve within 5-7 days
+
+                 2. Fever:
+                  - Patient reports a temperature of 102 degrees at home
+                 Plan:
+                  - Continue taking Tylenol as needed for fever and pain relief
+                  - Encourage adequate hydration and rest
+                  - Monitor temperature and return for follow-up if fever persists or worsens              
+
+              """
+    )
+    return prompt
+def get_medical_summary_prompt(notes_text):
+    prompt = f"Summarize the following meeting notes according to medical and health :\n\n{notes_text}\n\nSummary:"
+    return prompt
 
 # Function to query OpenAI GPT-4 for summarization
 def query_openai(prompt):
@@ -87,10 +225,8 @@ def query_openai(prompt):
             st.error(f"An error occurred with OpenAI: {e}")
         return None
 
-
 def run_summarizer_app():
     try:
-        # Streamlit App
         st.markdown(
             """
             <style>
@@ -115,7 +251,6 @@ def run_summarizer_app():
         )
 
         st.title("Meeting Notes Summarizer")
-        summary_type = st.text_input("Specify the type of summary you need (e.g., action items, conclusions)")
 
         # Sidebar for uploading multiple files
         st.sidebar.title("Upload Meeting Notes")
@@ -139,28 +274,37 @@ def run_summarizer_app():
             st.sidebar.write(f"**File Size:** {selected_file['size']} bytes")
             st.sidebar.write(f"**Original File Name:** {selected_file['name']}")
 
+        st.sidebar.title("Real-Time Transcription")
+        transcription = st.sidebar.checkbox("Enable Transcription")
+
+        transcribed_text = ""
+        if transcription:
+            transcribed_text = speech_to_text(language='en', use_container_width=True, just_once=True, key='STT')
+
         if st.button("Summarize") and selected_file:
-            file_extension = selected_file["name"].split('.')[-1]
-            if file_extension == "txt":
+            with st.spinner('Processing...'):
+             file_extension = selected_file["name"].split('.')[-1]
+             if file_extension == "txt":
                 notes_text = read_text_file(selected_file["path"])
-            elif file_extension == "xlsx":
+             elif file_extension == "xlsx":
                 notes_text = extract_text_from_xlsx(selected_file["path"])
-            elif file_extension == "pdf":
+             elif file_extension == "pdf":
                 notes_text = extract_text_from_pdf(selected_file["path"])
-            elif file_extension in ["mp3", "wav"]:
+             elif file_extension in ["mp3", "wav"]:
                 notes_text = extract_text_from_audio(selected_file["path"])
-            elif file_extension in ["mp4", "mkv", "avi"]:
+             elif file_extension in ["mp4", "mkv", "avi"]:
                 notes_text = extract_text_from_video(selected_file["path"])
-            else:
+             else:
                 st.error("Unsupported file type")
                 return
 
             with st.spinner('Summarizing...'):
-                prompt = f"Summarize in points the following meeting notes with a focus on {summary_type}:\n\n{notes_text}\n\nSummary:"
-                summary = query_openai(prompt)
-
+                summary_prompt = get_medical_summary_prompt(notes_text)
+                soap_notes_prompt = get_soap_notes_prompt(notes_text)
+                summary = query_openai(summary_prompt)
+                soap_notes = query_openai(soap_notes_prompt)
                 # Tabs for Transcription and Summary
-                tab1, tab2 = st.tabs(["Extracted Text", "Summary"])
+                tab1, tab2, tab3 = st.tabs(["Extracted Text", "Summary","Soap Notes"])
 
                 with tab1:
                     st.write(notes_text)
@@ -170,8 +314,31 @@ def run_summarizer_app():
                         st.write(summary)
                     else:
                         st.write("Could not generate summary for this file.")
+                with tab3:
+                    st.write(soap_notes)
+        elif transcribed_text is not None and len(transcribed_text) > 0:
+            summary_prompt = get_medical_summary_prompt(transcribed_text)
+            soap_notes_prompt = get_soap_notes_prompt(transcribed_text)
+            summary = query_openai(summary_prompt)
+            soap_notes = query_openai(soap_notes_prompt)
+            # Tabs for Transcription and Summary
+            tab1, tab2, tab3 = st.tabs(["Extracted Text", "Summary", "Soap Notes"])
+
+            with tab1:
+                st.write(transcribed_text)
+
+            with tab2:
+                if summary:
+                    st.write(summary)
+                else:
+                    st.write("Could not generate summary for this file.")
+            with tab3:
+                st.write(soap_notes)
+
 
     except Exception as e:
         st.error(f"An error occurred: {e}")
+
+
 
 run_summarizer_app()
